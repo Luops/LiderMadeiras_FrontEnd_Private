e4de0cbc9d95af0bf3f54dfbe7f87089d752cee9
fabricio rios <you@example.com>
2023-10-25 20:18:13 -0300

    Integrado função de excluir o produto.

--- src/components/listProducts/ListProducts.tsx
+++ src/components/listProducts/ListProducts.tsx
@@ -18,10 +18,47 @@ const ListProducts = (props: Props) => {
   const productsNotInPromotion = products.filter(
     (product: any) => !product.isPromotion
   );
+
+  // Encontre a quantidade máxima de itens na lista de produtos
+  const maxItemsProd = Math.max(productsNotInPromotion.length);
+  // Defina a altura do contêiner flexível pai com base na altura máxima da lista de produtos
+  const calculateHeightProd = () => {
+    if (maxItemsProd == 1) {
+      return `${maxItemsProd * 210}px`;
+    } else if (maxItemsProd >= 2) {
+      return `${maxItemsProd * 170}px`;
+    } else {
+      return "0px";
+    }
+  };
+
+  // Encontre a quantidade máxima de itens na lista de produtos
+  const maxItemsPromo = Math.max(productsInPromotion.length);
+  // Defina a altura do contêiner flexível pai com base na altura máxima da lista de promoção
+  const calculateHeightPromo = () => {
+    if (maxItemsPromo == 1) {
+      return `${maxItemsPromo * 210}px`;
+    } else if (maxItemsPromo >= 2) {
+      return `${maxItemsPromo * 170}px`;
+    } else {
+      return "0px";
+    }
+  };
+
+  // Altura do contêiner pai com base no número máximo de itens
+  const containerHeightProd = calculateHeightProd();
+
+  // Altura do contêiner pai com base no número máximo de itens
+  const containerHeightPromo = calculateHeightPromo();
+
+  console.log(products);
   return (
     <>
       <section className="flex gap-3 ml-[235px]">
-        <article className="flex flex-col w-[500px] bg-[#ededed] rounded-[10px_10px_0px_0px]">
+        <article
+          className="flex flex-col w-[500px] bg-[#ededed] rounded-[10px_10px_10px_10px] shadow-[5px_3px_20px_-4px_rgba(0,0,0,0.59)]"
+          style={{ height: containerHeightProd }}
+        >
           <h3 className="w-100 text-3xl font-bold text-white py-3 text-center rounded-[10px_10px_0px_0px] bg-gradient-to-r from-[#FE9022] to-orange-500">
             Produtos
           </h3>
@@ -30,13 +67,16 @@ const ListProducts = (props: Props) => {
             <ProductView key={product.id} {...product} />
           ))}
         </article>
-        <article className="flex flex-col w-[500px] bg-[#ededed] rounded-[10px_10px_0px_0px]">
+        <article
+          className="flex flex-col w-[500px] bg-[#ededed] rounded-[10px_10px_10px_10px] shadow-[5px_3px_20px_-4px_rgba(0,0,0,0.59)]"
+          style={{ height: containerHeightPromo }}
+        >
           <h3 className="w-100 text-3xl font-bold text-white py-3 text-center rounded-[10px_10px_0px_0px] bg-gradient-to-r from-[#FE9022] to-orange-500">
             Produtos em Promoção
           </h3>
           {/*Retornar produtos que sao promoções*/}
           {productsInPromotion.map((product: any) => (
-            <ProductView key={product.id} {...product} />
+            <ProductView key={product._id} {...product} />
           ))}
         </article>
       </section>
--- src/components/listProducts/productView/ProductView.tsx
+++ src/components/listProducts/productView/ProductView.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+// Components
+import ShowAlertDelete from "@/components/modals/ShowAlertDelete";
+
 // Icons
 import { IoTrashBinSharp, IoPencil } from "react-icons/io5";
 
 type Product = {
+  _id: string;
   title: string;
   description: string;
   price: string;
@@ -14,27 +18,66 @@ type Product = {
   url: string;
 };
 
-// Formato do preço
+// Formato do preço normal
 function formatProductPrice(price: string): string {
-  // Verifique se o preço contém um ponto decimal e duas casas decimais
-  if (!/\.\d{2}$/.test(price)) {
-    // Se não contiver, adicione um ponto decimal e duas casas decimais
-    return `${price}.00`;
+  // Converte o preço para um número de ponto flutuante
+  const priceAsFloat = parseFloat(price);
+
+  // Verifica se o preço é um número
+  if (!isNaN(priceAsFloat)) {
+    // Formata o preço com duas casas decimais
+    let formattedPrice = priceAsFloat.toFixed(2);
+
+    // Remove as casas decimais se forem zero
+    formattedPrice = formattedPrice.replace(/\.00$/, "");
+
+    return formattedPrice;
   }
+
+  // Retorna o preço original se não for um número
   return price;
 }
 
-// Formato do preço
+// Formato do preço da promoção
 function formatProductPromoPrice(promoPrice: string): string {
-  // Verifique se o preço contém um ponto decimal e duas casas decimais
-  if (!/\.\d{2}$/.test(promoPrice)) {
-    // Se não contiver, adicione um ponto decimal e duas casas decimais
-    return `${promoPrice}.00`;
+  // Converte o preço da promoção para um número de ponto flutuante
+  const promoPriceAsFloat = parseFloat(promoPrice);
+
+  // Verifica se o preço da promoção é um número
+  if (!isNaN(promoPriceAsFloat)) {
+    // Formata o preço da promoção com duas casas decimais
+    let formattedPromoPrice = promoPriceAsFloat.toFixed(2);
+
+    // Remove as casas decimais se forem zero
+    formattedPromoPrice = formattedPromoPrice.replace(/\.00$/, "");
+
+    return formattedPromoPrice;
   }
+
+  // Retorna o preço da promoção original se não for um número
   return promoPrice;
 }
 
+// Transforma o primeiro caractere de uma string em maiúsculo
+function capitalizeFirstLetter(text) {
+  return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+}
+
+// Transforma o primeiro caractere de uma string após um ponto em minúsculo
+function capitalizeAfterPeriod(text) {
+  return text.replace(
+    /\. [a-z]/g,
+    (match) => `. ${match.charAt(2).toUpperCase()}`
+  );
+}
+function formatDescription(text) {
+  const firstUpperCase = capitalizeFirstLetter(text);
+  const result = capitalizeAfterPeriod(firstUpperCase);
+  return result;
+}
+
 function ProductView({
+  _id,
   title,
   description,
   price,
@@ -44,38 +87,69 @@ function ProductView({
   promoPrice,
   url,
 }: Product) {
+  // State do modal de apagar
+  const [showDeleteModal, setShowDeleteModal] = React.useState(false);
+
   // Formate o preço
   const formattedPrice = formatProductPrice(price);
 
   // Formate o preço da promoção
   const formattedPromoPrice = formatProductPromoPrice(promoPrice);
+
+  // Limite máximo de caracteres para title e description
+  const maxTitleLength = 20; // Defina o valor desejado
+  const maxDescriptionLength = 50; // Defina o valor desejado
+
+  // Corte o título se ele exceder o limite e transforme a primeira letra maiuscula
+  const truncatedTitle =
+    title.length > maxTitleLength
+      ? `${capitalizeFirstLetter(title.slice(0, maxTitleLength))}...`
+      : capitalizeFirstLetter(title);
+
+  // Corte a descrição se ela exceder o limite e transforme a primeira letra maiuscula + primeira letra após ponto
+  const truncatedDescription =
+    description.length > maxDescriptionLength
+      ? `${formatDescription(description.slice(0, maxDescriptionLength))}...`
+      : formatDescription(description);
+
+  console.log(_id);
+
   return (
     <>
-      <article className="flex flex-col text-start px-2 py-1 gap-3">
+      {showDeleteModal && (
+        <ShowAlertDelete
+          setShowDeleteModal={setShowDeleteModal}
+          title={title}
+          _id={_id}
+        />
+      )}
+      <article className="flex flex-col text-start py-1 mx-2 gap-3 border-b border-[#fe902273] !h-[130px]">
         <div className="flex gap-2 justify-between">
           <div className="flex gap-2">
             <img
               src={url}
               alt={title}
-              className="h-[70px] max-w-[80px] bg-contain bg-center"
+              className="h-[70px] aspect-square bg-contain bg-center shadow-[5px_3px_20px_-4px_rgba(0,0,0,0.59)] rounded-lg"
             />
-            <div>
-              <h3 className="text-lg font-bold max-w-[280px]">{title}</h3>
-              <p className="text-sm text-justify text-[#666666] max-w-[280px]">
-                {description}
+            <div className="max-w-[250px]">
+              <h3 className="text-md font-bold line-clamp-1">
+                {truncatedTitle}
+              </h3>
+              <p className="text-sm text-justify text-[#666666] overflow-hidden break-words line-clamp-2">
+                {truncatedDescription}
               </p>
             </div>
           </div>
           {isPromotion && (
             <div className="pr-5">
-              <p className="text-2xl font-bold text-end">
+              <p className="text-xl font-bold text-end">
                 R$ {formattedPromoPrice}
               </p>
               <p className="text-md">R$ {formattedPrice}</p>
             </div>
           )}
           {!isPromotion && (
-            <p className="text-2xl font-bold pr-5 text-end">
+            <p className="text-xl font-bold pr-5 text-end">
               R$ {formattedPrice}
             </p>
           )}
@@ -84,20 +158,20 @@ function ProductView({
         <div className="flex justify-between">
           {/*Editar e Excluir */}
           <div className="flex gap-5 h-[100%] text-center justify-center">
-            <button onClick={() => alert("Excluiu")}>
-              <div className="bg-white p-2 rounded-full">
+            <button onClick={() => setShowDeleteModal(true)}>
+              <div className="bg-white p-2 rounded-full shadow-[0px_0px_9px_1px_#1b191929]">
                 <IoTrashBinSharp className="text-[20px] text-black hover:text-[#FF6E00] transition-colors ease-in-out duration-500" />
               </div>
             </button>
             <button onClick={() => alert("Editar")}>
-              <div className="bg-white p-2 rounded-full">
+              <div className="bg-white p-2 rounded-full shadow-[0px_0px_9px_1px_#1b191929]">
                 <IoPencil className="text-[20px] text-black hover:text-[#FF6E00] transition-colors ease-in-out duration-500" />
               </div>
             </button>
           </div>
           {/*Ver produto*/}
           <button
-            className="text-lg font-semibold bg-white px-3 rounded hover:bg-[#FF6E00] hover:text-white transition-colors ease-in-out duration-500"
+            className="text-lg font-semibold bg-white px-3 rounded hover:bg-[#FF6E00] hover:text-white transition-colors ease-in-out duration-500 shadow-[0px_0px_9px_1px_#1b191929]"
             onClick={() => alert("Ver")}
           >
             Ver
